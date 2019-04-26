3b2297b4fe04c549a0392497daf2b3ca0259dae6
Patrick Kim <pkim050@gmail.com>
2019-04-26 12:00:36 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,9 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    redirect_to @post and return if @post.update(post_params)
 
-    redirect_to post_path(@post)
+    render :edit
   end
 
   private
