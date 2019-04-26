d67bf4a12f8d03b38715191824d615fe3ddd1ea1
Patrick Kim <pkim050@gmail.com>
2019-04-26 12:30:37 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, uniqueness: true
   validates :phone_number, presence: true, length: { is: 10 }
 end
